4392bf0a676dea3386135dc4237369c866be3de9
AEM <a_emad@knights.ucf.edu>
2019-04-24 12:33:40 -0400

    A range of phase and bit numbers are subplotted
    The pdf file generated is more clear than the original plot.

--- HW3/HW3_Q2.py
+++ HW3/HW3_Q2.py
@@ -27,19 +27,31 @@ if __name__ == '__main__':
 	print("\nIt can be easily change by commenting Line36 and uncommenting Line37")
 	printsection()
 	printsection()
-	bitnum = int(input("Please Enter Bit Number: "))
-	phase = float(input("Please Enter the Phase: "))
-	inv_fourier_op= ifourier_op(bitnum)
-	state = phased_register(phase,bitnum)
-	phase_state = inv_fourier_op.dot(state)
-	probs = prob_amplitude(phase_state)
-	ax = plt.figure().gca()
-	ax.set_xlabel("$discrete phase$", fontsize=10)
-	ax.set_ylabel(r"$\mathbb{P}rob$", fontsize=10)
-	plt.title("The phase's real valued representation [0,1).")
-	plt.stem(np.arange(2**bitnum)/(2**bitnum),probs,'-.')
-	#plt.stem(np.arange(2 ** bitnum), probs, '-.')
-	#plt.plot(np.arange(2 ** bitnum) / 2 ** bitnum, probs)
+	#bitnum = int(input("Please Enter Bit Number: "))
+	i=0
+	bitnumr =6
+	phasenum =10
+	plt.figure(figsize= (50,50))
+	for phase in np.linspace(0,1,phasenum):
+		for bitnum in range(1,bitnumr):
+			print(phase)
+			i= i+1
+			#phase = float(input("Please Enter the Phase: "))
+			inv_fourier_op= ifourier_op(bitnum)
+			state = phased_register(phase,bitnum)
+			phase_state = inv_fourier_op.dot(state)
+			probs = prob_amplitude(phase_state)
+			ax = plt.subplot(phasenum,bitnumr-1,i)
+			#ax = plt.figure().gca()
+			ax.set_xlim(-0.5,1.5)
+			ax.set_ylim(0, 1.1)	
+			#ax.set_xlabel("$discrete phase$", fontsize=10)
+			#ax.set_ylabel(r"$\mathbb{P}rob$", fontsize=10)
+			plt.title("phase:{},bit={}".format(str(round(phase,2)),str(bitnum)))
+			plt.stem(np.arange(2**bitnum)/(2**bitnum),probs,'-.')
+			#plt.stem(np.arange(2 ** bitnum), probs, '-.')
+			#plt.plot(np.arange(2 ** bitnum) / 2 ** bitnum, probs)
+			#plt.show()
+	
+	plt.savefig("phase_bitnum.pdf", bbox_inches='tight')
 	plt.show()
-
-	pass
